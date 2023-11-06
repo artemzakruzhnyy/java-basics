c59da2cab17a18d92dce11b56394f7bd3ff67bbd
Artsiom_Zakruzhny <Artsiom_Zakruzhny@epam.com>
2023-11-07 00:29:11 +0100

    rollback

--- src/main/java/org/example/zakruzhny/Main.java
+++ src/main/java/org/example/zakruzhny/Main.java
@@ -4,5 +4,15 @@ public class Main {
     public static void main(String[] args) {
 
         Cat fiodr = new Cat();
+        Cat timofei = new Cat();
+
+        Hamster kuzma = new Hamster();
+        Hamster kuzia = new Hamster();
+
+        fiodr.say();
+        timofei.say();
+
+        kuzma.say();
+        kuzia.say();
     }
 }
