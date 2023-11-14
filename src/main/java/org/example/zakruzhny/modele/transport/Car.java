b39958b35420b1478eec4a40d749288c93e41780
Artsiom_Zakruzhny <Artsiom_Zakruzhny@epam.com>
2023-11-14 18:51:20 +0100

    updated class Car and Transport

--- src/main/java/org/example/zakruzhny/modele/transport/Car.java
+++ src/main/java/org/example/zakruzhny/modele/transport/Car.java
@@ -1,11 +1,22 @@
 package org.example.zakruzhny.modele.transport;
 
 public class Car extends Transport {
+
+    private String transmission;
+
     public Car(double speed) {
         super(speed);
     }
 
     public void startCarEngine(){
         super.startEngine();
+    }
+
+    public String getTransmission() {
+        return transmission;
+    }
+
+    public void setTransmission(String transmission) {
+        this.transmission = transmission;
     }
 }
--- src/main/java/org/example/zakruzhny/modele/transport/Transport.java
+++ src/main/java/org/example/zakruzhny/modele/transport/Transport.java
@@ -2,16 +2,38 @@ package org.example.zakruzhny.modele.transport;
 
 public class Transport {
     private double speed;
+    private String type;
+    private int year;
 
     public Transport(double speed){
         this.speed = speed;
+    }
+
+    void startEngine(){
+        System.out.println("Мотор завелся!");
     }
 
     public double getSpeed() {
         return speed;
     }
 
-    void startEngine(){
-        System.out.println("Мотор завелся!");
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
     }
 }
