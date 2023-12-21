080247ea5c3583c891b85e1d172c03d80bbf56c3
Artsiom_Zakruzhny <Artsiom_Zakruzhny@epam.com>
2023-12-21 19:12:20 +0100

    updated BitwiseOperators

---
+++ src/main/java/org/example/zakruzhny/operators/BitwiseOperators.java
@@ -0,0 +1,21 @@
+package org.example.zakruzhny.operators;
+
+public class BitwiseOperators {
+
+    public void makeBitwiseActions() {
+        /*
+        3 - 11
+        4 - 100
+        5 - 101
+        6 - 110
+        7 - 111
+        8 - 1000
+        9 - 1001
+        10 - 1010
+         */
+
+        System.out.println(5 | 6);
+        System.out.println(5 & 6);
+        System.out.println(5 ^ 6);
+    }
+}
