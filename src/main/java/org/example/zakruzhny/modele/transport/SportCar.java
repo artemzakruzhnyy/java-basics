c9e10ec02558941b144df4b5209a86f6ea3c34ce
Artsiom_Zakruzhny <Artsiom_Zakruzhny@epam.com>
2024-01-10 17:40:39 +0100

    added constructors to SportCar

--- src/main/java/org/example/zakruzhny/modele/transport/SportCar.java
+++ src/main/java/org/example/zakruzhny/modele/transport/SportCar.java
@@ -1,4 +1,13 @@
 package org.example.zakruzhny.modele.transport;
 
-public class SportCar extends Car{
+public class SportCar extends Car {
+
+    public SportCar(String name){
+        super(20.2);
+        System.out.println(name);
+    }
+
+    public SportCar(){
+
+    }
 }
